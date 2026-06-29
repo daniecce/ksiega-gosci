43171cf8c9bb9104926a2d07bbe195cd77d93d96
Daniel Cecelon <danielcecelon8@gmail.com>
2026-06-29 12:52:33 +0200

    fix: tytul strony

--- app/page.js
+++ app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
         <div className="text-center">
           <p className="text-5xl mb-4">💍</p>
           <h1 className="text-white text-2xl font-semibold">
-            Wesele Karoliny i Daniel
+            Wesele Karoliny i Daniela
           </h1>
           <p className="text-gray-200 text-sm mt-2">
             11 Lipca 2026 · Miodowy Zakątek
